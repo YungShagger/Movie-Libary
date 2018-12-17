225ccd8cceea0a7e5dbcfe51d3d42a17f290050d
YungShagger <daniellmagyar@gmail.com>
2018-12-17 10:03:52 +0100

    Box office revenue has been added to the data storage

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
-pass
+import display, file_link, statistics, data
+
+
+
+def clear_screen():
+    print("\n" * 80)
+
+def choose():
+    option = []
+    
+    while True:
+        try:
+            pass
+
+def main():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
