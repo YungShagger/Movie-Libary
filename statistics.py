e4301e784f84615c5ea4874b4b69c00c55dff8ae
Mateut Krisztina <mateutkrisztina@gmail.com>
2018-12-17 10:05:34 +0100

    functions defs added to statistics

--- statistics.py
+++ statistics.py
@@ -1,1 +1,18 @@
-pass
+def sort_movies_by_stars_number(movies)
+
+def sort_movies_by_release_year(movies)
+
+def sort_movies_by_length(movies)
+
+def get_longest_movie(movies)
+
+def get_shortest_movie(movies)
+
+def get_movie_by_director(movies, director)
+
+def get_movie_by_star(movies, star)
+
+def get_movie_by_year(movies, year)
+
+def get_movie_of_highest_revenue(movies)
+
